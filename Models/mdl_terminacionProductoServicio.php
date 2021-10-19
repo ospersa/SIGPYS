1f0828c52db9e55d5b092cdcfdf5ae01e0ed910b
ospersa <ospersa@gmail.com>
2021-10-19 15:21:45 -0500

    Update mdl_terminacionProductoServicio.php

--- Models/mdl_terminacionProductoServicio.php
+++ Models/mdl_terminacionProductoServicio.php
@@ -72,7 +72,7 @@
                                         <th>Fecha prevista entrega</th>
                                         <th>Fecha creación</th>
                                         <th>Metadata y cierre</th>
-                                        <!--<th>Terminar y enviar correo</th>-->
+                                        <th>Terminar y enviar correo</th>
                                     </tr>
                                 </thead>
                                 <tbody>';
@@ -153,7 +153,7 @@
                                         <td>'.$fechPrev.'</td>
                                         <td>'.$fechSol.'</td>
                                         <td class="center-align"><a href="#modalTerminarProSer" data-position="left" class="modal-trigger tooltipped" data-tooltip="Más información del Producto/Servicio" onclick="envioData(\'INF'.$idSol.'\',\'modalTerminarProSer.php\')"><i class="material-icons '.$colorMeta.'-text">info_outline</i></a></td>
-                                        <!-- <td><a href="#'.$modal.'" data-position="left" class="modal-trigger tooltipped" data-tooltip="'.$mjsTooltip.'" '.$onclick.'><i class="material-icons '.$color.'-text">done_all</i></a></td> -->
+                                        <td><a href="#'.$modal.'" data-position="left" class="modal-trigger tooltipped" data-tooltip="'.$mjsTooltip.'" '.$onclick.'><i class="material-icons '.$color.'-text">done_all</i></a></td>
                                     </tr>'; 
                     }    
                 $string .= '    </tbody>
